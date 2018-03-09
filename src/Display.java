0898593f6fe2798edeaa718636150ae092c4a450
vivien <593748781@qq.com>
2018-03-09 10:49:34 +0800

    添加了README.md

--- src/Display.java
+++ src/Display.java
@@ -1,9 +1,5 @@
 
 
-
-
-
-
 public class Display {
 
 
--- src/Main.java
+++ src/Main.java
@@ -6,120 +6,64 @@ import java.util.ArrayList;
 import flowerSql.*;
 
 public class Main {
-
-
     public static void main(String[] args) {
         FlowerSQL flower = new FlowerSQL();
         flower.Init();
         flower.mainCircle();
-
-
-
-
-
-
-        /*
-        //学生表
-        Database database = new Database();
-        Table table = new Table();
-
-        ArrayList<Field> fields = new ArrayList<>();
-        fields.add(new Field("id", "int", new ArrayList<>()));
-        fields.add(new Field("name", "char(30)", new ArrayList<>()));
-        fields.add(new Field("age", "int", new ArrayList<>()));
-        fields.add(new Field("sex", "bool", new ArrayList<>()));
-
-
-        ArrayList<String> temp = new ArrayList<>();
-        temp.add("name");
-        table.setStructOfTable(new StructOfTable("Student",fields, "id", temp));
-
-        // Set Data
-        Data data = new Data();
-
-        ArrayList<String> row1 = new ArrayList<>();
-        row1.add("1");
-        row1.add("DaMing");
-        row1.add("18");
-        row1.add("True");
-        data.addSingleData(row1);
-
-
-        ArrayList<String> row2 = new ArrayList<>();
-        row2.add("2");
-        row2.add("XiaoMing");
-        row2.add("9");
-        row2.add("True");
-        data.addSingleData(row2);
-
-        ArrayList<String> row3 = new ArrayList<>();
-        row3.add("3");
-        row3.add("DaHong");
-        row3.add("18");
-        row3.add("False");
-        data.addSingleData(row3);
-
-        table.setDataOfTable(data);
-
-
-
-        ArrayList<Table> tables = new ArrayList<>();
-        tables.add(table);
-
-        //用户表
-        Table userTable = new Table();
-
-        ArrayList<Field> userFields = new ArrayList<>();
-
-        ArrayList<String> idConstraints = new ArrayList<>();
-        idConstraints.add("PrimaryKey");
-        ArrayList<String> nameConstraints = new ArrayList<>();
-        nameConstraints.add("ForeignKey");
-        nameConstraints.add("Not Null");
-        userFields.add(new Field("id", "int", idConstraints));
-        userFields.add(new Field("name", "char", nameConstraints));
-        userFields.add(new Field("password", "char", new ArrayList<>()));
-
-        ArrayList<String> userTemp = new ArrayList<>();
-        userTemp.add("name");
-        userTable.setStructOfTable(new StructOfTable("User",userFields, "id", userTemp));
-
-        Data userData = new Data();
-
-        ArrayList<String> userRow1 = new ArrayList<>();
-        userRow1.add("1");
-        userRow1.add("DaMing");
-        userRow1.add("123456");
-        userData.addSingleData(userRow1);
-
-        ArrayList<String> userRow2 = new ArrayList<>();
-        userRow2.add("2");
-        userRow2.add("XiaoMing");
-        userRow2.add("qazwsx");
-        userData.addSingleData(userRow2);
-
-        userTable.setDataOfTable(userData);
-
-
-        tables.add(userTable);
-        database.setTables(tables);
-
-        WriteFile writeFile = new WriteFile();
-        writeFile.saveAllData(database);
-
-
-        ReadFile readFile = new ReadFile();
-        readFile.getAllTableData();
-        readFile.getAllTableStruct();
-
-
-        SubService subService = new SubService(database);
-
-        subService.checkPassword("DaMing","123456");
-
-        */
     }
 }
+/*
+【create】
+create table tableName(
+attribute1 type1,
+attribute2 type2,
+……
+)
+create index tableName on table(desc);
+例：
+create index stub on Student(attribute desc/asc);
+
+【help】
+help database/table/index/view;
+
+【select】
+select (distinct) attribute1,attribute2……/* from tableName where xxxx;
+select (distinct) attribute1,attribute2……/* from tableName group by attribute;
+例：
+select id,chinese,math,english from leeTest where id>=1 order by id dec;
+select * from tableName group by id;
+select distinct * from leeTest;
+
+【insert】
+insert into tableName values(value1,value2……); 
+insert into tableName(attribute1,attribute2……) values(value1,value2……); 
+例如：
+insert into Student values(99,Tom,17,true); 
+insert into Student(id,name,age,sex) values(34,Lily,15,false);
+
+【grant】
+grant attribute1,attribute2…… to user1,user2……/public;
+例如：
+grant insert,delete,select to public;
+grant delete to root;
+
+【revoke】
+revoke attribute1,attribute2…… from user1,user2……/public;
+例如：
+revoke select from flower;
+
+【update】
+update tableName set xxxx (where xxxx);
+例如：
+update leeTest set chinese=chinese*2;
+update leeTest set chinese=1 where id=1 or id=2;
+
+【delete】
+delete from tableName (where xxxx);
+例如：
+delete from leeTest where id=1;
+delete from leeTest;
+*/
 
 
 
